1cf7b846ea16edd12082cff7997fca650cf7d468
Strucker.eth <aberamacarleton@gmail.com>
2022-11-12 11:34:52 +0300

    static class methods

---
+++ static-class-methods.js
@@ -0,0 +1,13 @@
+var exam1 = 98;
+var exam2 = 86;
+var exam3 = 66;
+    
+
+
+class Marks{
+   static totalMarks(a,b,c){
+        return (a+b+c);
+    }
+}
+
+ console.log(Marks.totalMarks(exam1,exam2,exam3));
