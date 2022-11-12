7c24a07aec4cee67c66a5b99279d7fe28264b0e8
Strucker.eth <aberamacarleton@gmail.com>
2022-11-12 13:11:45 +0300

    Project Quiz Definition

--- finalProject.js
+++ finalProject.js
@@ -1,3 +1,12 @@
+/*
+You are making a text encryptor. It should take multiple words and output a combined version, where each word is separated by a dollar sign $.
+For example, for the words "hello", "how", "are", "you", the output should be "$hello$how$are$you$".
+The given code declares a class named Add, with a constructor that takes one rest parameter.
+Complete the code by adding a print() method to the class, which should generate the requested output.
+*/
+
+
+
 class Add {
     constructor(...words) {
         this.words = words;
