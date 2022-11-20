47250ccf0e26faf9b20fcba6be62e9dba4a7a89c
Strucker.eth <aberamacarleton@gmail.com>
2022-11-20 23:17:25 +0300

    Quiz--definition

--- SQL/AVG function.sql
+++ SQL/AVG function.sql
@@ -1,2 +1,8 @@
+/*You are given the following table sam_grades, which shows Sam’s exam scores.
+Write a query to output the average of Sam's exam scores for the first semester. */
+
+
 SELECT AVG(score) FROM sam_grades 
 WHERE semester = 1;
+
+
