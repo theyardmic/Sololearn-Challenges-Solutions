1341861cd3428dc24d4b2928959f7a57b21d016d
Strucker.eth <aberamacarleton@gmail.com>
2022-11-20 23:22:54 +0300

    QUiz --definition

--- SQL/JOIN with WHERE.sql
+++ SQL/JOIN with WHERE.sql
@@ -1,3 +1,7 @@
+/*Write a query to output all of the students with their teachers' last names in one table,
+ sorted by students ID. */
+
+
 SELECT students.id,students.firstname, students.lastname,
  teachers.lastname AS teacher FROM students, teachers
 WHERE students.teacherid = teachers.id
