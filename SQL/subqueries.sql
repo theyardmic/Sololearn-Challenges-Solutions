3b6a7a5c7acef4990387032692e0a4fc44d0ab8e
Strucker.eth <aberamacarleton@gmail.com>
2022-11-20 23:26:53 +0300

    -- def

--- SQL/subqueries.sql
+++ SQL/subqueries.sql
@@ -1,6 +1,7 @@
 /*
-SELECT name, category, price, fatpercentage FROM Foods 
-WHERE fatpercentage < (SELECT AVG(fatpercentage) FROM foods);
+Help Monica lose weight by writing a query to choose the foods, 
+whose fat percentages are lower than the average from the "Foods" table.
+Then show the resulting table.
 */
 
 SELECT name, category, price, fatpercentage FROM Foods 
