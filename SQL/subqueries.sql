420080101ed6ea73a7c18d4a4f99aa23f35bbf75
Strucker.eth <aberamacarleton@gmail.com>
2022-11-20 23:25:50 +0300

    QUiz definition

--- SQL/subqueries.sql
+++ SQL/subqueries.sql
@@ -1,2 +1,7 @@
+/*
+SELECT name, category, price, fatpercentage FROM Foods 
+WHERE fatpercentage < (SELECT AVG(fatpercentage) FROM foods);
+*/
+
 SELECT name, category, price, fatpercentage FROM Foods 
 WHERE fatpercentage < (SELECT AVG(fatpercentage) FROM foods);
