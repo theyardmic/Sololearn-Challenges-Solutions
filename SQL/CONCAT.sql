8fbd9843ba37c5eebc613f2a145f1f5eeac6870e
Strucker.eth <aberamacarleton@gmail.com>
2022-11-20 23:19:12 +0300

    Quiz --definition

--- SQL/CONCAT.sql
+++ SQL/CONCAT.sql
@@ -1,3 +1,5 @@
+/*Write a query to output the firstname and lastname columns into one column named fullname separated by space, and the total annual salary for each employee keeping in mind bonuses named 'total'.
+ Sort by the 'total' column. */
 SELECT CONCAT("firstname", ' ',"lastname") as fullname,
 (experience*500) + (salary*12) as total FROM staff
 ORDER BY total
