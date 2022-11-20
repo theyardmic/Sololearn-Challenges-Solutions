2f04cc0df308306955888b0cbd43e473171972db
Strucker.eth <aberamacarleton@gmail.com>
2022-11-20 23:24:19 +0300

    Quiz definition

--- SQL/LOGICAL OPERATORS.sql
+++ SQL/LOGICAL OPERATORS.sql
@@ -1,3 +1,6 @@
+/*Write a query to output the names of all of the films which were produced by Marvel Studios 
+in 2010 or later, sorted by the 'name' column. */
+
 SELECT name FROM films 
 WHERE production = 'Marvel Studios' AND year >= 2010
 ORDER BY name;
